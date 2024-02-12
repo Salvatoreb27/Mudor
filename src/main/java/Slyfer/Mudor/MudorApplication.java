04bc7804b7b97306c963f10c0686d77c3ed8c799
Salvatoreb27 <138437593+Salvatoreb27@users.noreply.github.com>
2024-02-12 11:14:48 +0100

    start

---
+++ src/main/java/Slyfer/Mudor/MudorApplication.java
@@ -0,0 +1,13 @@
+package Slyfer.Mudor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Slyfer/Mudor/MudorApplicationTests.java
@@ -0,0 +1,13 @@
+package Slyfer.Mudor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
